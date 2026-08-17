9e5767b5cb4ebf26c8b44cb12f9bd6e5a42b2d19
Jalal Sadoon Hameed <jalal.hameed@uobaghdad.edu.iq>
2026-08-17 11:12:39 +0300

    Delete desktop-app directory

--- desktop-app/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('appAuth', {
-  checkPassword: (password) => ipcRenderer.invoke('check-app-password', password),
-});
